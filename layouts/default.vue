774327fb1ddc31d07db4c47a1387ffc05ab8b2c7
kratos619 <gaurav.palaspagar@hotmail.com>
2019-04-19 22:35:04 +0530

    add Side Nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav"></TheHeader>
+    <TheSidenav :show="displaySideNav" @close="displaySideNav = false"></TheSidenav>
     <nuxt/>
   </div>
 </template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySideNav: false
+    };
+  }
+};
+</script>
 
 <style>
 html,
